e5b46d323d66f3f3fd85214e526e4c2bf9a3437b
Cacapon Project <cacapon.project@gmail.com>
2025-07-21 17:40:54 +0900

    主機能の実装

--- src/main.ts
+++ src/main.ts
@@ -1,85 +1,58 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, normalizePath, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface AutoDeleteSettings {
+	targetFolder: string;
+	ttlMinites: number;
+	checkInterval: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AutoDeleteSettings = {
+	targetFolder: "_",
+	ttlMinites: 0,
+	checkInterval: 60,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutoTmpFilesRemover extends Plugin {
+	settings: AutoDeleteSettings;
+	intervalId: number | null = null;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.startInterval();
+		this.addSettingTab(new AutoTmpFilesRemoverSettingTab(this.app, this));
 	}
 
 	onunload() {
 
+	}
+
+	startInterval() {
+		this.runCleanUp();
+		const intervalMs = toMilliSec(this.settings.checkInterval);
+		this.intervalId = window.setInterval(() => this.runCleanUp(), intervalMs);
+	}
+
+	async runCleanUp() {
+		if (this.settings.checkInterval === 0) return ;
+		const folderPath = normalizePath(this.settings.targetFolder);
+		const folder = this.app.vault.getAbstractFileByPath(folderPath);
+		if (!(folder instanceof TFolder)) return;
+
+		const now = Date.now();
+		const ttlMillis = toMilliSec(this.settings.ttlMinites);
+		const delFiles: string[] = [];
+		for (const file of folder.children) {
+			if (file instanceof TFile && file.extension === "md") {
+				const ctime = file.stat.ctime;
+				if (now - ctime > ttlMillis) {
+					await this.app.vault.delete(file);
+					delFiles.push(file.path);
+				}
+			}
+		}
+		if (delFiles.length > 0) {
+			new Notice(`Deleted files: \n${delFiles.join("\n")}`);
+		}
 	}
 
 	async loadSettings() {
@@ -91,44 +64,78 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AutoTmpFilesRemoverSettingTab extends PluginSettingTab {
+	plugin: AutoTmpFilesRemover;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AutoTmpFilesRemover) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
-
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
+		containerEl.createEl("h2", { text: "Auto TmpFiles Remover"});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("対象フォルダ")
+			.setDesc("削除対象とするフォルダの相対パス")
+			.addText((text) =>
+				text
+					.setPlaceholder("tmp")
+					.setValue(this.plugin.settings.targetFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.targetFolder = value.trim();
+						await this.plugin.saveSettings();}
+					)
+			);
+		
+		new Setting(containerEl)
+			.setName("TTL (分)")
+			.setDesc("ファイル作成から何分後に削除するか")
+			.addText((text) =>
+				{
+					text.inputEl.type = "number";
+					text.inputEl.min = "1";
+					text.inputEl.max = "10080";
+					text
+						.setPlaceholder("1")
+						.setValue(String(this.plugin.settings.checkInterval))
+						.onChange(async (value) => {
+							const numVal = parseInt(value);
+							if (!isNaN(numVal) && numVal >= 1 && numVal <= 10080){ 
+								this.plugin.settings.ttlMinites = numVal;
+								await this.plugin.saveSettings();
+							}
+						});
+				}
+			);
+
+		new Setting(containerEl)
+			.setName("チェック間隔(分)")
+			.setDesc("何分毎に削除処理を実行するか (0-1440) ※0の場合は実行しません")
+			.addText((text) =>
+				{
+					text.inputEl.type = "number";
+					text.inputEl.min = "0";
+					text.inputEl.max = "1440";
+					text
+						.setPlaceholder("0")
+						.setValue(String(this.plugin.settings.checkInterval))
+						.onChange(async (value) => {
+							const numVal = parseInt(value);
+							if (!isNaN(numVal) && numVal >= 0 && numVal <= 1440){ 
+								this.plugin.settings.checkInterval = numVal;
+								await this.plugin.saveSettings();
+								this.plugin.onunload();
+								this.plugin.startInterval();
+							}
+						});
+				}
+			);
 	}
+}
+
+function toMilliSec(minitues: number): number {
+	return minitues * 60000;
 }
